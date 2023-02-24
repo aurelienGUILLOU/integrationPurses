9a24e06d2fcff26d2f8ba7d86e1f11f368f98e3a
aurelcode <aurelien.guillou@edu.univ-fcomte.fr>
2023-02-24 14:07:32 +0100

    hi
    hi

---
+++ src/test/java/integration/PurseCodeSecretIntegrationTest.java
@@ -0,0 +1,50 @@
+package integration;
+
+import argent.Purse;
+import argent.TransactionRejetterException;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import verrou.CodeSecret;
+
+public class PurseCodeSecretIntegrationTest {
+
+    Purse purse;
+    CodeSecret codeSecret;
+    private double montantCredit = 1000;
+    private String goodCode;
+    private String badCode;
+    private double montantDebit = 50;
+
+
+    @BeforeEach
+    public void setUp(){
+        codeSecret = new CodeSecret();
+        purse = new Purse(codeSecret);
+        purse.credite(montantCredit);
+        goodCode = codeSecret.reveleCode();
+        badCode = goodCode.substring(0,2)+(goodCode.charAt(3)!='0'?'0':'1');
+    }
+
+    @Test
+    public void testDebitAvecCodeJusteAutorisé() throws Exception {
+        purse.debite(montantDebit, goodCode);
+        Assertions.assertEquals(montantCredit-montantDebit, purse.getSolde());
+    }
+
+    @Test
+    public void testDebitAvecCodeFauxRejeté(){
+        Assertions.assertThrows(TransactionRejetterException.class,
+                ()->purse.debite(montantDebit, badCode));
+    }
+
+    @Test
+    public void testDebitAvecCodeBloquéRejeté(){
+
+    }
+
+    @Test
+    public void testCreditAvecCodeBloquéRejeté(){
+
+    }
+}
